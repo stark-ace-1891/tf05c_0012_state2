190356402f51fa8721448a9c757c8e2dd6e26f18
Cesar Morales <cesar.aml1891@gmail.com>
2023-04-30 10:43:32 -0500

    Agregando Appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  Laptop pepito = Laptop(
-    color: "Azus",
-    marca: "Azul",
-  );
-  Laptop juan = Laptop(
-    color: "Azus",
-    marca: "Azul",
-  );
-  print(pepito);
-  print(pepito.color);
+  runApp(MyApp());
+}
 
-  List<Laptop> laptops = [];
-  List<int> numeros = [10, 20, 30, 40];
-
-  laptops.add(pepito);
-  laptops.add(juan);
-  laptops.add(Laptop(
-    color: "Azus",
-    marca: "Dell",
-  ));
-
-  print(laptops);
-
-  for (int item in numeros) {
-    print(item + 1000);
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
   }
+}
 
-  numeros.forEach((element) {
-    print(element);
-  });
-
-  laptops.forEach((item) {
-    print(item.marca);
-  });
-
-  Telefono miTelefono = Telefono(marca: "Motorola", numero: 9696585852);
-
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.redAccent,
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+        title: Text(
+          "Listado General",
+        ),
       ),
-    ),
-  );
-}
-
-class Laptop {
-  //Atributos
-  String marca;
-  String color;
-
-  //Constructor
-  Laptop({required this.marca, required this.color});
-
-  //Metodos
-  void encender() {
-    print("Encendiendo Laptop");
+      body: Column(
+        children: [],
+      ),
+    );
   }
-}
-
-class Telefono {
-  String marca;
-  int numero;
-  Telefono({required this.marca, required this.numero});
 }
